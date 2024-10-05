a07aef56eef387dbac567554e53b616e881399dc
JDeWeese <jdeweese574@icloud.com>
2024-10-05 08:48:39 -0500

    Initial Commit

---
+++ Blue Arc/Blue_ArcApp.swift
@@ -0,0 +1,17 @@
+//
+//  Blue_ArcApp.swift
+//  Blue Arc
+//
+//  Created by Joseph DeWeese on 10/5/24.
+//
+
+import SwiftUI
+
+@main
+struct Blue_ArcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Blue ArcTests/Blue_ArcTests.swift
@@ -0,0 +1,16 @@
+//
+//  Blue_ArcTests.swift
+//  Blue ArcTests
+//
+//  Created by Joseph DeWeese on 10/5/24.
+//
+
+import Testing
+
+struct Blue_ArcTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
